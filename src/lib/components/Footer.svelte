8af5a41c98746a1bdaf7550e4a66e7ef4ae8aadb
Liam Grossman <liam.j.grossman@gmail.com>
2025-08-23 13:55:40 -0400

    Format + switch to cloudflare

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,18 +1,18 @@
-<script lang='ts'>
-	import { LINKS } from "$lib/utilities/constants";
+<script lang="ts">
+	import { LINKS } from '$lib/utilities/constants';
 </script>
 
-<div class="justify-center text-gray-500 space-x-3 flex ease-in-out mb-7">
-      <a href={LINKS.GITHUB} target="_blank" rel="noopener noreferrer">
-        <p class="hover:text-gray-800">GitHub</p>
-      </a>
-      <a href={LINKS.LINKEDIN} target="_blank" rel="noopener noreferrer">
-        <p class="hover:text-gray-800">LinkedIn</p>
-      </a>
-      <a href={LINKS.RESUME} target="_blank" rel="noopener noreferrer">
-        <p class="hover:text-gray-800">Résumé</p>
-      </a>
-      <a href="mailto:me@liamgrossman.com">
-        <p class="hover:text-gray-800">Email</p>
-      </a>
-    </div>
+<div class="mb-7 flex justify-center space-x-3 text-gray-500 ease-in-out">
+	<a href={LINKS.GITHUB} target="_blank" rel="noopener noreferrer">
+		<p class="hover:text-gray-800">GitHub</p>
+	</a>
+	<a href={LINKS.LINKEDIN} target="_blank" rel="noopener noreferrer">
+		<p class="hover:text-gray-800">LinkedIn</p>
+	</a>
+	<a href={LINKS.RESUME} target="_blank" rel="noopener noreferrer">
+		<p class="hover:text-gray-800">Résumé</p>
+	</a>
+	<a href="mailto:me@liamgrossman.com">
+		<p class="hover:text-gray-800">Email</p>
+	</a>
+</div>
